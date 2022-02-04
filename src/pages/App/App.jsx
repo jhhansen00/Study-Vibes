7f8c63ae18dff8ad08867b2317deb2af88c33993
Jack Hansen <jhhansen00@gmail.com>
2022-02-04 12:27:00 -0600

    Initial commit for Study Vibes

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import { useState } from "react";
+import { getUser } from "../../utilities/users-service";
+import { Routes, Route } from "react-router-dom";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import AuthPage from "../AuthPage/AuthPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import NavBar from "../../components/NavBar/NavBar";
+
+function App() {
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className="App">
+      {
+        user ?
+        <>
+        <NavBar setUser={setUser} user={user}/>
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser} />
+      }
+    </main>
+  );
+}
+
+export default App;
