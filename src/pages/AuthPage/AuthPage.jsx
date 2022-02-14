f1ad3e781d976e9152ab6e4f01884bacd29d7de2
Jack Hansen <jhhansen00@gmail.com>
2022-02-13 18:10:06 -0600

    Tiny tweak of authpage

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -4,8 +4,9 @@ import LoginForm from "../../components/LoginForm/LoginForm";
 function AuthPage({ setUser }) {
   return (
     <main>
-      <h1>Sign-In | Log-In</h1>
+      <h1>Sign-In</h1>
       <SignUpForm setUser={setUser} />
+      <h1>Log-In</h1>
       <LoginForm setUser={setUser} />
     </main>
   )
